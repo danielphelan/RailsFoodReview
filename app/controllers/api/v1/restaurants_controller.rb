462cf80582e2de32a194940f8ea54d7e589d3aa6
danielphelan <danielphelan06@hotmail.com>
2015-04-10 22:17:27 +0100

    Restaurants API

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::RestaurantsController < ApplicationController
+   respond_to :json
+
+            def index
+              @restaurants = Restaurant.all
+              respond_with(@restaurants)
+
+            end
+            def show
+                @restaurant = Restaurant.find(params[:id])
+                respond_with (@restaurant)
+            end
+
+            def create
+                @restaurant = Restaurant.new(rest_params)
+                if @restaurant.save
+                    respond_with @restaurant
+                else
+                end
+              end
+               
+            
+
+            private
+
+                def rest_params
+                    params.require(:restaurant).permit(:name,:food, :address)
+
+                end
+            end
--- app/views/devise/sessions/create.json.jbuilder
+++ app/views/devise/sessions/create.json.jbuilder
@@ -1,4 +1,6 @@
 json.id @resource.id.to_s
 json.name @resource.name
 json.email @resource.email
+json.confirmed_at @resource.confirmed_at
+
 json.message 'Logged in'
