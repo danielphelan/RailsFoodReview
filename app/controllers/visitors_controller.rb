e36628a4a709bc49ac4e5541934d18b9f81ccc23
danielphelan <danielphelan06@hotmail.com>
2015-04-14 23:43:53 +0100

    Change home on login

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,8 @@
 class VisitorsController < ApplicationController
+  def index
+    if user_signed_in?
+      @reviews = Review.all.paginate(page: params[:page], per_page: 5)
+      render 'reviews/index'
+    end
+  end
 end
